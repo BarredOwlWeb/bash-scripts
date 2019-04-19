d84e9288a850b26788c175faa5c2e288b2fd3bb2
David White <david@developcents.com>
2019-04-19 10:02:09 -0400

    Finalize changes to make the Server Backup script work as intended.

--- backup-server.sh
+++ backup-server.sh
@@ -6,25 +6,29 @@ YESTERDAY=$(date --date="1 day ago" +%Y%m%d)
 if [[ $TODAY == "Sun" ]]; then
 	# Delete anything older than 35 days
 	find -mindepth 1 -maxdepth 1 -type d -mtime +35 -exec rm -rf {} \;
-	mkdir -p /volume1/Host_backups/Example/server.example.com/backups-$DATE-SUN/home
-	rsync -aPH --link-dest=/volume1/Host_backups/Example/server.example.com/backups-$YESTERDAY -e "ssh -i /root/.ssh/id_rsa" --delete backup@server.example.com:/mnt/backups/* /volume1/Host_backups/Example/server.example.com/backups-$DATE-SUN
-  
-	rsync -aPH --link-dest=/volume1/Host_backups/Example/server.example.com/backups-$YESTERDAY/home --rsync-path="sudo rsync" -e "ssh -i /root/.ssh/id_rsa" --exclude "virtfs" --delete backup@server.example.com:/home/ /volume1/Host_backups/Example/server.example.com/backups-$DATE-SUN/home/
 
 	# Delete any other directories that are older than 2 days, that do not contain "SUN" in the dir name
+        find -mindepth 1 -maxdepth 1 -type d -mtime +2 ! \( -name "*SUN*" \) -exec rm -rf {} \;
+
+	# Set $DIR_NAME and $LINK_DEST variables
+	DIR_NAME=$TODAY-SUN
+	LINK_DEST=$YESTERDAY
+	
+elif [[ $TODAY == "Mon" ]]; then
+	DIR_NAME=$TODAY
+	LINK_DEST=$(date --date="2 days ago" +%Y%m%d)
 	find -mindepth 1 -maxdepth 1 -type d -mtime +2 ! \( -name "*SUN*" \) -exec rm -rf {} \;
 
-else
-	mkdir -p /volume1/Host_backups/Example/server.example.com/backups-$DATE/home
-
-	if [[ $TODAY == "Mon" ]]; then
-
-	else
-        
-	rsync -aPH --link-dest=/volume1/Host_backups/Example/server.example.com/backups-$YESTERDAY -e "ssh -i /root/.ssh/id_rsa" --delete backup@server.example.com:/mnt/backups/* /volume1/Host_backups/Example/server.example.com/backups-$DATE
-        rsync -aPH --link-dest=/volume1/Host_backups/Example/server.example.com/backups-$YESTERDAY/home --rsync-path="sudo rsync" -e "ssh -i /root/.ssh/id_rsa" --exclude "virtfs" --delete backup@server.example.com:/home/ /volume1/Host_backups/Example/server.example.com/backups-$DATE/home/
-
+else 
+	DIR_NAME=$TODAY
+	LINK_DEST=$YESTERDAY
+	find -mindepth 1 -maxdepth 1 -type d -mtime +2 ! \( -name "*SUN*" \) -exec rm -rf {} \;
 fi
 
-find -mindepth 1 -maxdepth 1 -type d -mtime +2 ! \( -name "*SUN*" \) -exec rm -rf {} \;
-chown -R Example.users /volume1/Host_backups/Example/server.example.com/
+# Make backup directory
+mkdir -p /volume1/Host_backups/Example/server.example.com/backups-$DIR_NAME/home
+
+# Run the backups	
+rsync -aPH --link-dest=/volume1/Host_backups/Example/server.example.com/backups-$LINK_DEST -e "ssh -i /root/.ssh/id_rsa" --delete backup@server.example.com:/mnt/backups/* /volume1/Host_backups/Example/server.example.com/backups-$DIR_NAME
+
+rsync -aPH --link-dest=/volume1/Host_backups/Example/server.example.com/backups-$LINK_DEST/home --rsync-path="sudo rsync" -e "ssh -i /root/.ssh/id_rsa" --exclude "virtfs" --delete backup@server.example.com:/home/ /volume1/Host_backups/Example/server.example.com/backups-$DIR_NAME/home/
