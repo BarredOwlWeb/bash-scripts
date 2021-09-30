cd523f2bd06b01b1c147329e4ad557e11b28e73e
Dave Smith <dave@sophoservices.com>
2021-09-30 15:45:50 -0500

    Add Retention variables

--- backupdb.sh
+++ backupdb.sh
@@ -32,6 +32,10 @@
 #LOG=$mysql-backup.log
 #exec > $LOG 2>&1
 
+DAILY_RETENTION=+0 # how many 24 hour periods to keep locally
+WEEKLY_RETENTION=+1 # home many 24 hour periods to keep in weekly folder
+FOLDER=/mnt/backups/mariadb
+PASS="CHANGE-ME"
 DATABASES=$(echo 'show databases;' | mysql -u backup --password='CHANGE-ME' | grep -v ^Database$)
 LIST=$(echo $DATABASES | sed -e "s/\s/\n/g")
 DATE=$(date +%Y%m%d)
@@ -45,22 +49,23 @@ if [[ $i != "mysql" ]]; then
 	fi
 
 	# redirect stderr to stdout, then redirect the regular stdout to the backup file. Order is important
-	OUTPUT=`/bin/nice mysqldump --single-transaction $i 2>&1 > /mnt/backups/mysql/daily/$i.$DATE.sql `
+	OUTPUT=`/bin/nice mysqldump --single-transaction --password=$PASS $i 2>&1 > $FOLDER/daily/$i.$DATE.sql `
 	if [[ $OUTPUT != "" ]]; then
 		echo Problem with database "$i"
 		echo ====
 		echo $OUTPUT
 	fi
 
-	find /mnt/backups/mysql/daily/* -type f -mtime +0 -exec rm -f {} \;
+	# remove files older than 24 hrs
+	find $FOLDER/daily/* -type f -mtime $DAILY_RETENTION -exec rm -f {} \;
 
 	if [[ $SUNDAY == "Sun" ]]; then
-		cp /mnt/backups/mysql/daily/$i.$DATE.sql /mnt/backups/mysql/weekly/$i.$DATE.sql
-		find /mnt/backups/mysql/weekly/* -type f -mtime +1 -exec rm -f {} \;
+		cp $FOLDER/daily/$i.$DATE.sql $FOLDER/weekly/$i.$DATE.sql
+		find $FOLDER/weekly/* -type f -mtime $WEEKLY_RETENTION -exec rm -f {} \;
 	fi
 fi
 
-find /mnt/backups/mysql/daily/ -name "*.sql" -exec gzip -f {} \;
-find /mnt/backups/mysql/weekly/ -name "*.sql" -exec gzip -f {} \;
-chown -R backup.backup /mnt/backups/mysql
+find $FOLDER/daily/ -name "*.sql" -exec gzip -f {} \;
+find $FOLDER/weekly/ -name "*.sql" -exec gzip -f {} \;
+chown -R backup.backup $FOLDER
 done
